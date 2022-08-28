87454510ffbbe883f36bd967a0f0ceb42bb8951c
Sri Vishnuvardhan A <53358337+vishnuswmech@users.noreply.github.com>
2022-08-28 08:01:14 +0530

    Create factorial.js

---
+++ Javascript/Assignment_1/factorial.js
@@ -0,0 +1,19 @@
+
+let number=5
+let i = `${number}`-1
+let factorial
+
+if (number==0)
+ number=1
+if (number==1)
+ number=1
+if (number<0)
+ console.log("The Factorial for negative number does not exist")
+while (i>=1){
+
+    number=number*i
+    --i
+    
+
+}
+console.log(`The factorial for given number is ${number}`)
