7f408eae54a39fda14c3ee4b119e1cf39a9294a9
Sri Vishnuvardhan A <53358337+vishnuswmech@users.noreply.github.com>
2022-08-30 17:31:33 +0530

    Create sum_of_multiples_of_3_and_5_below_1000.js

---
+++ Javascript/Assignment_2/sum_of_multiples_of_3_and_5_below_1000.js
@@ -0,0 +1,20 @@
+"use strict"
+
+let i=0 // initialize increment as 0
+let sum=0 // initialize sum as 0
+while(i<1000) { // range below 1000
+   
+    if ((i%3===0 || i%5===0)){ // multiples of 3 or multiples of 5
+             
+        sum=sum+i   // Increment value stored in sum
+
+    }
+    ++i   // Pre-increment
+    
+}
+console.log(`The sum of multiples of 3 and 5 are ${sum}`)
+
+/*
+Output:
+The sum of multiples of 3 and 5 are 233168
+*/
