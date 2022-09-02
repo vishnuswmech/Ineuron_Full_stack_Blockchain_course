d2d4bc6cfd539f5f2b39a5c85d4c235a1781f167
Sri Vishnuvardhan A <53358337+vishnuswmech@users.noreply.github.com>
2022-09-02 17:10:59 +0530

    Create array_push.js

---
+++ Javascript/Self-Practice/array_push.js
@@ -0,0 +1,12 @@
+array_of_numbers=[1,2,3,4,5]
+array_of_numbers_after_push = array_of_numbers.push(10)
+console.log(array_of_numbers)
+console.log(array_of_numbers_after_push)
+
+/*
+
+The output would be
+[ 1, 2, 3, 4, 5, 10 ]
+6
+
+*/
