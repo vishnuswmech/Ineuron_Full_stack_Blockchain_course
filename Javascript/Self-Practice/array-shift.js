cef90357dc44a19997b80c0f3424890fa1a97ff0
Sri Vishnuvardhan A <53358337+vishnuswmech@users.noreply.github.com>
2022-09-02 17:28:06 +0530

    Create array-shift.js

---
+++ Javascript/Self-Practice/array-shift.js
@@ -0,0 +1,12 @@
+array_of_numbers=[1,2,3,4,5]
+array_of_numbers_after_shift = array_of_numbers.shift()
+console.log(array_of_numbers)
+console.log(array_of_numbers_after_shift)
+
+/*
+
+The output will be
+[ 2, 3, 4, 5 ]
+1
+
+*/
