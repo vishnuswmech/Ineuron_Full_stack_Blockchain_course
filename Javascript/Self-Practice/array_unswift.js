50266b2da7bffe6a9f1b49c9f809ba80ff887cbf
Sri Vishnuvardhan A <53358337+vishnuswmech@users.noreply.github.com>
2022-09-02 17:50:24 +0530

    Create array_unswift.js

---
+++ Javascript/Self-Practice/array_unswift.js
@@ -0,0 +1,12 @@
+array_of_numbers=[1,2,3,4,5]
+array_of_numbers_after_unshift = array_of_numbers.unshift(10)
+console.log(array_of_numbers)
+console.log(array_of_numbers_after_unshift)
+
+/*
+
+The output will be
+[ 10, 1, 2, 3, 4, 5 ]
+6
+
+*/
