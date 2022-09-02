1714d8617c5c72ba8a3ca5965388f7b6c5f8bad3
Sri Vishnuvardhan A <53358337+vishnuswmech@users.noreply.github.com>
2022-09-02 17:23:44 +0530

    Create array_pop.js

---
+++ Javascript/Self-Practice/array_pop.js
@@ -0,0 +1,12 @@
+array_of_numbers=[1,2,3,4,5]
+array_of_numbers_after_pop = array_of_numbers.pop()
+console.log(array_of_numbers)
+console.log(array_of_numbers_after_pop)
+
+/*
+
+The output would be
+[ 1, 2, 3, 4 ]
+5
+
+*/
