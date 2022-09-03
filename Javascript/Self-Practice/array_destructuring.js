537830ff69978140cee87bec1a3b6130979ffd2d
Sri Vishnuvardhan A <53358337+vishnuswmech@users.noreply.github.com>
2022-09-03 20:31:04 +0530

    Create array_destructuring.js

---
+++ Javascript/Self-Practice/array_destructuring.js
@@ -0,0 +1,7 @@
+let array = [2, 4, 6, 8, 10]
+let [element1, element2, element3, element4, element5] = array
+console.log(element1)
+console.log(element2)
+console.log(element3)
+console.log(element4)
+console.log(element5)
