5f4563dfc192572363a4c70b974cc8ba43ede574
Sri Vishnuvardhan A <53358337+vishnuswmech@users.noreply.github.com>
2022-09-03 20:28:49 +0530

    Create forEach_loop.js

---
+++ Javascript/Self-Practice/forEach_loop.js
@@ -0,0 +1,9 @@
+//Without arrow function
+let array_of_numbers = [1, 2, 3, 4, 5]
+array_of_numbers.forEach(function(value) {
+console.log(value);
+})
+
+//With arrow function
+let array_of_numbers = [1, 2, 3, 4, 5]
+array_of_numbers.forEach(value => console.log(value))
