0ca5564a16fdac59d1bb1cd1c0460c96294adc9f
Sri Vishnuvardhan A <53358337+vishnuswmech@users.noreply.github.com>
2022-09-03 20:27:18 +0530

    Create splice_with_replacement_values.js

---
+++ Javascript/Self-Practice/splice_with_replacement_values.js
@@ -0,0 +1,4 @@
+let array_of_numbers=[1,2,3,4,5,6,7,8,9,10]
+let array_of_numbers_after_splice=array_of_numbers.splice(4,5,11,12)
+console.log(array_of_numbers)
+console.log(array_of_numbers_after_splice)
