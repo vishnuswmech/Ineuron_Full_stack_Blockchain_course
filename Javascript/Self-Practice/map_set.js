1b7862875c23925870988671a17ccd57fdcae4ce
Sri Vishnuvardhan A <53358337+vishnuswmech@users.noreply.github.com>
2022-09-04 19:36:10 +0530

    Create map_set.js

---
+++ Javascript/Self-Practice/map_set.js
@@ -0,0 +1,15 @@
+let map = new Map()
+
+map.set("vishnu", "js")
+map.set("vishnu", "java")
+
+
+//console.log(map.values())
+map.forEach((key) => console.log(key))
+
+/*
+retuns only the value
+output would be
+java
+
+*/
